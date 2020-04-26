792be526d76b91766667791c7c01196bb364cfd7
saipullanwar <hiccup.221200@gmail.com>
2020-04-26 20:09:33 +0800

    Buat Class OperasiString

---
+++ src/string/OperasiString.java
@@ -0,0 +1,13 @@
+package string;
+
+public class OperasiString   {
+    public static void main(String[] args) {
+        String kota = "Tanah Bumbu";
+
+        System.out.println(kota);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+    }
+}
