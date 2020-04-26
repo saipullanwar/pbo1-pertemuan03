b727909d911468561b1fedb6ef8de463bec968e1
saipullanwar <hiccup.221200@gmail.com>
2020-04-26 22:58:24 +0800

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,25 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("aku");
+                break;
+            case 2:
+                System.out.println("mama");
+                break;
+            case 3:
+                System.out.println("abah");
+                break;
+            case 4:
+                System.out.println("kaka");
+                break;
+            default:
+                System.out.println("orang lain");
+                break;
+                        
+        }
+    }
+}
