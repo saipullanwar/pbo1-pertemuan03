f5475940fe82cd0de3936d8ad08fae72be42d13a
saipullanwar <hiccup.221200@gmail.com>
2020-04-26 22:51:39 +0800

    Percabangan IFelse

--- src/percabangan/IFElse.java
+++ src/percabangan/IFElse.java
@@ -2,13 +2,16 @@ package percabangan;
 
 public class IFElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang,kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang cukup, mantap sanak");
+            System.out.println("Uang pas.. Uwu..");
         }
     }
 }
